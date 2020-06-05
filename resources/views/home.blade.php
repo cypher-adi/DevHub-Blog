c6ea51afdd896a6fdad5bd752fbd73a206942b7a
Cypher <adityasaha444@gmail.com>
2020-06-05 15:09:27 +0530

    Profile homepage design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,34 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="/img/cypher.jpg" style="width:150px;" class="rounded-circle ">
+        </div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>saha_adi_</h1>
             </div>
+            <div class="d-flex">
+                <div class="pr-4"><strong>44</strong> Posts</div>
+                <div class="pr-4"><strong>373</strong> Followers</div>
+                <div class="pr-4"><strong>391</strong> Following</div>
+            </div>
+            <div class='pt-4 font-weight-bold'>Aditya Kumar</div>
+            <div>aka "Cypher" | Computer Geek | Web Developer</div>
+            <div><a href="#">www.cypher-resume.herokuapp.com</a></div>
         </div>
     </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://instagram.fjai5-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.110.928.928a/s640x640/96659418_262291735135612_3457119677470427431_n.jpg?_nc_ht=instagram.fjai5-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=rPl49vzvMZkAX9rqFc2&oh=964935f13d658a2176a4260fa3997d12&oe=5F026072" class='w-100'>
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fjai5-1.fna.fbcdn.net/v/t51.2885-15/e35/c64.0.548.548a/100898919_639147106680041_7770796487488702501_n.jpg?_nc_ht=instagram.fjai5-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=v4kbt3wt28MAX-ZOpVp&oh=3a2518c19356bfb25e24c4aad941d647&oe=5F052E05" class='w-100'>
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fjai5-1.fna.fbcdn.net/v/t51.2885-15/e35/c127.0.495.495a/100950429_172849147497410_8469189274443807895_n.jpg?_nc_ht=instagram.fjai5-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=hT6lRmol6CMAX9KQ2mW&oh=c75cd2cf1e217a6aa9de83947e27257f&oe=5F050284" class='w-100'>
+        </div>
+    </div>  
 </div>
 @endsection
