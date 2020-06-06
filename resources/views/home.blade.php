5e888d985a5211c11f7a75daeed84e0821431ea9
Cypher <adityasaha444@gmail.com>
2020-06-06 14:31:12 +0530

    profile route + username fetched

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
         </div>
         <div class="col-9 pt-5">
             <div>
-                <h1>saha_adi_</h1>
+                <h1> {{ $user->username }} </h1>
             </div>
             <div class="d-flex">
                 <div class="pr-4"><strong>44</strong> Posts</div>
