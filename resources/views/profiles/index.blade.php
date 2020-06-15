e3971d6b403240da995db76214841076f6fe9c93
Cypher <adityasaha444@gmail.com>
2020-06-15 17:15:18 +0530

    all post rendered in profile

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,7 +9,7 @@
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1> {{ $user->username }} </h1>
-                <a href="#" class="btn btn-primary">Add New Post</a>
+                <a href="/p/create" class="btn btn-primary">Add New Post</a>
             </div>
             <div class="d-flex">
                 <div class="pr-4"><strong>44</strong> Posts</div>
@@ -22,15 +22,11 @@
         </div>
     </div>
     <div class="row pt-5">
+        @foreach($user->posts as $post)
         <div class="col-4">
-            <img src="https://instagram.fjai5-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.110.928.928a/s640x640/96659418_262291735135612_3457119677470427431_n.jpg?_nc_ht=instagram.fjai5-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=rPl49vzvMZkAX9rqFc2&oh=964935f13d658a2176a4260fa3997d12&oe=5F026072" class='w-100'>
+            <img src="/storage/{{ $post->image }}" class='w-100'>
         </div>
-        <div class="col-4">
-            <img src="https://instagram.fjai5-1.fna.fbcdn.net/v/t51.2885-15/e35/c64.0.548.548a/100898919_639147106680041_7770796487488702501_n.jpg?_nc_ht=instagram.fjai5-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=v4kbt3wt28MAX-ZOpVp&oh=3a2518c19356bfb25e24c4aad941d647&oe=5F052E05" class='w-100'>
-        </div>
-        <div class="col-4">
-            <img src="https://instagram.fjai5-1.fna.fbcdn.net/v/t51.2885-15/e35/c127.0.495.495a/100950429_172849147497410_8469189274443807895_n.jpg?_nc_ht=instagram.fjai5-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=hT6lRmol6CMAX9KQ2mW&oh=c75cd2cf1e217a6aa9de83947e27257f&oe=5F050284" class='w-100'>
-        </div>
+        @endforeach
     </div>  
 </div>
 @endsection
