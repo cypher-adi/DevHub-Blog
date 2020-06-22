35fb0268a30ef51d8ac599054ac92e08964a6c0f
Cypher <adityasaha444@gmail.com>
2020-06-22 10:56:21 +0530

    Profile edit policy added

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -16,6 +16,8 @@ class ProfilesController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user->profile);
+        
         return view('profiles.edit', compact('user'));
     }
 
@@ -28,7 +30,8 @@ class ProfilesController extends Controller
             'image' => ''
         ]);
 
-        $user->profile->update($data);
+        auth()->user()->profile->update($data);
+
         return redirect("/profile/{$user->id}");
     }
 }
