acbbf6812e868e48c2b883e06445680e0cb988bb
Cypher <adityasaha444@gmail.com>
2020-06-30 08:06:49 +0530

    Default Profile Pic added

--- app/Profile.php
+++ app/Profile.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {   
     protected $guarded = [];
+
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : '/profile/tBvJBst7LnoovPM2cnAAvpW4yfBmC1CdWGS1kGJS.png';
+        return '/storage/'.$imagePath;
+    }
     
     public function user()
     {
